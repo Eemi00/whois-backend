b7cc45ac426c983bdcade53d00b8e92c1ccc1d48
Eemi <illikaineneemij@gmail.com>
2026-01-22 11:27:07 +0200

    Fixed running bug

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from api import get_whois_data
+from fastapi.middleware.cors import CORSMiddleware
 
 # Määritetään app FastAPIn kautta
 app = FastAPI()
 
 # Merkataan sivuston ROOT tällä koodille
 @app.get("/")
-def home():
-    return {"message": "Domain API is running"}
+def read_root():
+    return {"status": "API is online", "docs": "/docs"}
 
 # Testataan haku tällä
 @app.get("/search")
 def search(domain: str):
-    message = get_whois_data(domain)
-    return {"status": message}
+    return get_whois_data(domain)
